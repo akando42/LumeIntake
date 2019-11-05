05b418e17a9e4d23358d66b68719eb0040e2191a
Vladimir Slavchev <engineers@infinitylab.world>
2019-11-05 14:45:32 -0600

    Load APIs

--- src/components/AdminPage/index.js
+++ src/components/AdminPage/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Admin, Resource, ListGuesser } from 'react-admin';
-import simpleRestProvider from 'ra-data-simple-rest';
-const dataProvider = simpleRestProvider('http://localhost:9000');
+import jsonServerProvider from 'ra-data-json-server';
+
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 const AdminPage = () => (
   <Admin dataProvider={dataProvider}>
-        <Resource name="users-read-all" list={ListGuesser} />
+        <Resource name="users" list={ListGuesser} />
   </Admin>
-);
+)
 
 export default AdminPage;
