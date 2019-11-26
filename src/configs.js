c578e0fd84ed0d23da05ba9b3065fa505d38c1cf
Vladimir Slavchev <engineers@infinitylab.world>
2019-11-26 17:11:04 -0600

    Updates

--- src/configs.js
+++ src/configs.js
@@ -1,3 +1,3 @@
 export const CMS_API_TOKEN = 'daa138494164d1e636da8e27df38ca';
 export const CMS_API_URL = 'https://cms.lume-wellness.com/';
-export const LAMBDA_API = 'http://localhost:9000';
+export const LAMBDA_API = 'https://lumechicago.com/.netlify/functions';
