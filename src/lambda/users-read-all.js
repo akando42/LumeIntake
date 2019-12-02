780ed37eb929b1a533d6c0949d9f19b83a69086b
Vladimir Slavchev <engineers@infinitylab.world>
2019-12-02 10:41:33 -0600

    Update Promise functions

--- src/lambda/users-read-all.js
+++ src/lambda/users-read-all.js
@@ -1,28 +1,58 @@
 import db from './server'
 import User_Details from './users-model'
 
+function preflight(callback){
+  callback(null, {
+    statusCode: 204, 
+    headers: {
+      'Content-Type':'application/json',
+      'Access-Control-Allow-Origin':'*',
+      'Access-Control-Allow-Headers':'*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT',
+    },
+    body: {},
+  });
+}
+
 exports.handler = async (event, context, callback) => {
-  try {
-    var users_detail = await User_Details.find()
-    var response = {
-      msg: "These are users data",
-      data: users_detail
-    }
-    return {
-      statusCode: 200, 
-      headers: {
-        'Content-Type':'application/json',
-        'X-Total-Count':users_detail.length,
-      }, 
-      body: JSON.stringify(response)
-    }
-  } catch (err) {
-    return {
-      statusCode: 500,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({msg: err.message})
+  context.callbackWaitsForEmptyEventLoop = false 
+  if (event.httpMethod === 'OPTIONS'){
+    preflight(callback);
+  } else {
+    try {
+      await User_Details.find({},function(err, data){
+        if(err){
+          callback(null, {
+            statusCode: 500,
+            headers: {
+              'Content-Type': 'application/json',
+              'Access-Control-Allow-Origin': '*',
+              'Access-Control-Request-Headers': '*',
+            },
+            body: JSON.stringify({msg: err.message})
+          })
+        }
+        callback(null, {
+          statusCode: 200, 
+          headers: {
+            'Content-Type':'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Request-Headers': '*',
+            'X-Total-Count':data.length
+          }, 
+          body: JSON.stringify(data)
+        })
+      })
+    } catch (err) {
+      callback(null, {
+        statusCode: 500,
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Request-Headers': '*',
+        },
+        body: JSON.stringify({msg: err.message})
+      })
     }
   }
 }
