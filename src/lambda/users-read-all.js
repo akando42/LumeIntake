8743a5f8b2484fb6e1386727c1373304ffd0ea48
Vladimir Slavchev <engineers@infinitylab.world>
2019-12-02 09:15:32 -0600

    Update with Promises

--- src/lambda/users-read-all.js
+++ src/lambda/users-read-all.js
@@ -8,21 +8,21 @@ exports.handler = async (event, context, callback) => {
       msg: "These are users data",
       data: users_detail
     }
-    return callback(null, {
+    return {
       statusCode: 200, 
       headers: {
         'Content-Type':'application/json',
         'X-Total-Count':users_detail.length,
       }, 
       body: JSON.stringify(response)
-    })
+    }
   } catch (err) {
-    return callback(null, {
+    return {
       statusCode: 500,
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({msg: err.message})
-    })
+    }
   }
 }
